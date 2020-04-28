cc91579c2af365abcf05c861978b206e92bb4af1
Nick Marcopoli <nick@marcopo.li>
2020-04-27 22:30:40 -0400

    clean code

--- showdown/battle_bots/harijo/main.py
+++ showdown/battle_bots/harijo/main.py
@@ -20,59 +20,7 @@ class BattleBot(Battle):
         move = self.pick_move_from_battles(battles)
         return format_decision(self, move)
 
-    def top_quartile_strat(self, root):
-        # WHEN YOU'RE BEHIND
-        best25percentileNode = None
-        for myMove in root.children:
-            moveScores = []
-            for opMove in myMove.children:
-                moveScores.append(opMove.children[0])
-            moveScores.sort(key=lambda x: float(x.name))
-            # get the top quarter percentile score
-            topQuarter = moveScores[int(len(moveScores) * .75)]
-            print(f"Top Quartile for {topQuarter.parent.parent.name}: {topQuarter.name}")
-            s = 0
-            for m in moveScores:
-                s += float(m.name)
-            print(f"Average for that: {s / len(moveScores)}")
-            print()
-            if best25percentileNode == None or float(topQuarter.name) > float(best25percentileNode.name):
-                best25percentileNode = topQuarter
-        
-        return best25percentileNode.parent.parent.name
-
-    def top_3_moves_strat(self, root):
-        # Perform depth first search
-        # our modification for this milestone was taking a randomization of the top 3 safest moves
-        top3Score = []
-        for myMove in root.children:
-
-            for opMove in myMove.children:
-                # the only child of opMove is the score
-                # until we have our three top moves, just add any move
-                if len(top3Score) < 3:
-                    top3Score.append(opMove.children[0])
-                elif float(opMove.children[0].name) == float(min(top3Score, key=lambda x: float(x.name)).name):
-                    # if score is the same, randomly choose to add to top 3
-                    # avoid determenistically favoring later in alphabet moves
-                    if bool(randint(0,1)):
-                        top3Score.remove(min(top3Score, key=lambda x: float(x.name)))
-                        top3Score.append(opMove.children[0])
-                    #if better move is found, we remove the minimum value from the top 3.
-                elif float(opMove.children[0].name) > float(min(top3Score, key=lambda x: float(x.name)).name):
-                    top3Score.remove(min(top3Score, key=lambda x: float(x.name)))
-                    top3Score.append(opMove.children[0])
-
-        # print our results
-        print("top 3 choices:")
-        for move in top3Score:
-            print(move.parent.parent.name, move.name)
-        print("")
-        shuffle(top3Score)
-        return top3Score.pop().parent.parent.name
-
     def aggressive_pick(self, root, safety):
-        # WHEN YOU'RE BEHIND
         bestAveNode = None
         bestAve = -float('inf')
         for myMove in root.children:
@@ -83,11 +31,8 @@ class BattleBot(Battle):
             s = 0
             for m in moveScores:
                 s += float(m.name)
-            # print(f"Average for {moveScores[0].parent.parent.name}: {s / len(moveScores)}")
             s += safety*int(min(moveScores, key=lambda x: float(x.name)).name)
             weightedAve = s / (len(moveScores) + safety)
-            # print(f"Weighted average for {moveScores[0].parent.parent.name}: {weightedAve}")
-            # print()
             if weightedAve > bestAve:
                 bestAveNode = moveScores[0]
                 bestAve = weightedAve
@@ -97,35 +42,11 @@ class BattleBot(Battle):
         
         return bestAveNode.parent.parent.name
 
-    def safest_pick(self, root):
-        # WHEN YOU'RE AHEAD
-        safestNode = None
-        safestMin = -float('inf')
-        for myMove in root.children:
-            moveScores = []
-            for opMove in myMove.children:
-                moveScores.append(opMove.children[0])
-            
-            minScore = int(min(moveScores, key=lambda x: float(x.name)).name)
-            # print(f"Min Score for {moveScores[0].parent.parent.name}: {minScore}")
-            # print()
-            if minScore > safestMin:
-                safestNode = moveScores[0]
-                safestMin = minScore
-            elif minScore == safestMin and bool(randint(0,1)):
-                safestNode = moveScores[0]
-                safestMin = minScore
-        
-        return safestNode.parent.parent.name
-
     def pick_move_from_battles(self, battles):
         # Only work on current battle
         # In practice the bot can play several at once, for now we simplified to one battle.
         battle = battles[0]
         root = Node("Root")
-
-        # with open('battleFile.obj', 'wb') as f:
-        #     pickle.dump(battle, f)
 
         state = battle.create_state()
         mutator = StateMutator(state)
@@ -177,31 +98,6 @@ class BattleBot(Battle):
         oppTotalHP += unseenPoke
         oppTotalHP *=100
 
-        """
-        POKEMON_STATIC_STATUSES = {
-            constants.FROZEN: -40,
-            constants.SLEEP: -25,
-            constants.PARALYZED: -25,
-            constants.TOXIC: -30,
-            constants.POISON: -10,
-            None: 0
-        }
-        POKEMON_BOOST_DIMINISHING_RETURNS = {
-        -6: -3.3,
-        -5: -3.15,
-        -4: -3,
-        -3: -2.5,
-        -2: -2,
-        -1: -1,
-        0: 0,
-        1: 1,
-        2: 2,
-        3: 2.5,
-        4: 3,
-        5: 3.15,
-        6: 3.30,
-    }
-        """
         possibleStatuses = {
             "psn" : .06,
             "frz" : .25,
@@ -275,16 +171,11 @@ class BattleBot(Battle):
         buff_aggression_modifier = (myBuffs - oppBuffs) * buff_aggression_multiplier
         print(f"Buff modifier:{buff_aggression_modifier}")
 
-        # if myTotalHP > oppTotalHP:
-        #     print("We are ahead, bot will play safe")
-        #     bot_choice = self.safest_pick(root)
-        # else:
-        #the higher the safety constant, the more likely we will choose the safest move. The lower, the more aggressive our bot will play
         safety = (3 + status_aggression_modifier + buff_aggression_modifier) * myTotalHP/oppTotalHP
         if safety < 0:
             print("WARNING: safety constant is less than 0, changing to 0.1")
             safety = 0.1
-        print(f"we are behind, bot will play aggressively with safety constant of {safety}")
+        print(f"bot will play with safety constant of {safety}")
         bot_choice = self.aggressive_pick(root,safety)
         print(f"choice: {bot_choice}")
         print(f"the safest pick was {self.safest_pick(root)}")
